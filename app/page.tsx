fa343fd9f013828b4510406a1a39173d24a2e650
Andrew <andrewanselmo@me.com>
2024-02-24 12:31:02 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@
 import CountButton from "./CountButton"
 import SearchBar from "./SearchBar/SearchBar"
 import Header from "./Header"
+import "./Header.css"
 export default function Page(){
     return <div>
 
     {/* below is a counter button and a github link */}
 
-     {/* <CountButton name = {"+1"} mult ={1}/>
-     <CountButton name = {"+2"} mult = {2}/>
-     <a className = "tag" href ="https://github.com/andrewanselmo/next" > my github repo </a> 
-      */}
+
+    
+     
     
 
     <Header/>
      <SearchBar/>
+
+     <a id = "link" href ="https://github.com/andrewanselmo/next" > Github Repo </a> 
     </div>
 }
